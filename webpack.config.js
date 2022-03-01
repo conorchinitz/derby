506d2a7edc007eb1d138c978ac160752974c72ee
Conor Chinitz <48924938+conorchinitz@users.noreply.github.com>
2022-03-01 16:01:28 -0500

    Install webpack-dev-server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Derby - Development',
+      meta: { 'viewport': 'width=device-width, initial-scale=1' },
+    })
+  ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
   },
   module : {
     rules: [
