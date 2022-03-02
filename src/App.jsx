3d90f7c729f12637a4ba2f56b6f0325cc461294e
Conor Chinitz <48924938+conorchinitz@users.noreply.github.com>
2022-03-02 15:55:08 -0500

    Render plaintext with React.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Render me, please and thank you.</div>
+    );
+  }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Derby',
+      template: './src/index.html',
       meta: { 'viewport': 'width=device-width, initial-scale=1' },
     })
   ],
