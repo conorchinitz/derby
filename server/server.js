8050a471ca0c123917199f8809e97c01c1aa392d
Conor Chinitz <48924938+conorchinitz@users.noreply.github.com>
2022-03-02 00:36:18 -0500

    Display all games on page load (VERY rudimentary UI).

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const gamesController = require('./controllers/gamesController.js');
+const express = require('express')
+const path = require('path')
+const gamesController = require('./controllers/gamesController.js')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', gamesController.getGames, (req, res) => {
-  res.status(200).json(res.locals.games);
+app.use(express.static('dist'))
+
+app.get('/', (req, res) => 
+  res.status(200).sendFile(path.join(__dirname, '../dist/index.html')))
+
+app.get('/games/all', gamesController.getGames, (req, res) => {
+  res.status(200).json(res.locals.games)
 })
 
 
 
 // Global error-handler
 app.use((err, req, res, next) => {
-  res.send('ERROR');
+  res.send('ERROR')
 })
 
 
@@ -23,4 +29,4 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}.`)
 })
 
-module.exports = app;
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -10,4 +10,27 @@ document.addEventListener('DOMContentLoaded', function main(){
   code.src = Code;
 
   document.body.append(code);
-})
+
+  fetch('/games/all')
+    .then(data => data.json())
+    .then(data => {
+      data.forEach((game) => {
+        const { game_id, power1, power2, winning_power, player1, player2, winning_player } = game;
+        
+        const gameDiv = document.createElement('div');
+        const gameInfo = document.createElement('p');
+
+        gameInfo.innerText = 
+          `Game #${game_id}
+          ${power1} vs. ${power2}
+          (${player1} vs. ${player2})
+          Winner: ${winning_power} (${winning_player})`;
+
+          gameDiv.append(gameInfo);
+          document.body.append(gameDiv);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
