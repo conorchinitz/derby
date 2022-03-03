6b64bb663b68f769f5e800bbeec09d14e21f4957
Conor Chinitz <48924938+conorchinitz@users.noreply.github.com>
2022-03-03 18:05:37 -0500

    Create 'Add Game' page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './components/Header';
-import { GamesContainer } from './containers/GamesContainer';
+import GamesContainer from './containers/GamesContainer';
+import GameAdder from './components/GameAdder';
 
 import BoxCover from './assets/santorini-box-cover.webp';
 
-function App() {
-  return (
-    <div id="app">
-      <Header />
-      <div id="box-cover-container">
-        <img id="box-cover-img" src={BoxCover} alt="The box cover of the game Santorini." />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'add game',
+      form: {
+        power1: '',
+        power2: '',
+        player1: '',
+        player2: '',
+        winning_power: '',
+        winning_player: '',
+      }
+    };
+    this.addGame = this.addGame.bind(this);
+    this.updateForm = this.updateForm.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+  }
+
+  render() {
+    let content;
+    switch (this.state.mode) {
+      case 'display games': {
+        content = <GamesContainer addGame={this.addGame}/>;
+        break;
+      }
+      case 'add game': {
+        content = <GameAdder formData={this.state.form} resetForm={this.resetForm}/>;
+        break;
+      }
+    }
+
+    return (
+      <div id="app">
+        <Header />
+        <div id="box-cover-container">
+          <img id="box-cover-img" src={BoxCover} alt="The box cover of the game Santorini." />
+        </div>
+        {content}
       </div>
-      <GamesContainer />
-    </div>
-  );
+    );
+  }
+
+  addGame() {
+    this.setState({ mode: 'add game' });
+  }
+
+  displayGames() {
+    this.setState( {mode: 'display games' });
+  }
+
+  resetForm(e) {
+    e.preventDefault();
+    this.setState({
+      form: {
+        power1: '',
+        power2: '',
+        player1: '',
+        player2: '',
+        winning_power: '',
+        winning_player: '',
+      }
+    });
+  }
+
+  updateForm(e) {
+    console.log(e.target.textContent);
+  }
 }
 
 export default App;
---
+++ src/components/GameAdder.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class GameAdder extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div id="game-adder">
+        <form action="post" autoComplete="off">
+          <label htmlFor="power1">Power 1</label>
+          <input id="power1" type="text" autoFocus/>
+          <label htmlFor="power2">Power 2</label>
+          <input id="power2" type="text" />
+          <label htmlFor="player1">Player 1</label>
+          <input id="player1" type="text" />
+          <label htmlFor="player2">Player 2</label>
+          <input id="player2" type="text" />
+          <label htmlFor="winning_power">Winning Power</label>
+          <input id="winning_power" type="text" />
+          <label htmlFor="winning_player">Winning Player</label>
+          <input id="winning_player" type="text" />
+          <div id="game-adder-buttons">
+            <button id="start-over" onClick={this.props.resetForm}>Start Over</button>
+            <button>Submit !</button>
+          </div>
+        </form>
+
+      </div>
+    );
+  }
+}
+
+
+export default GameAdder;
