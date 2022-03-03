da13147b4c23193c4c68ba23215e4db9e8f22819
Conor Chinitz <48924938+conorchinitz@users.noreply.github.com>
2022-03-03 03:13:45 -0500

    Add a lighter font color for secondary-info.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,16 +5,16 @@ function Game(props) {
   return (
     <div className="game">
       <div>
-        <p>Game #{props.game_id}</p>
+        <p className="secondary">Game #{props.game_id}</p>
       </div>
       <div className="powers">
         <p className="clickable" onClick={() => props.handleClick(props.power1)}>{props.power1}</p>
-        <p>vs.</p>
+        <p className="secondary">vs.</p>
         <p className="clickable" onClick={() => props.handleClick(props.power2)}>{props.power2}</p>
       </div>
       <div className="players">
         <p className="clickable" onClick={() => props.handleClick(props.player1)}>({props.player1}</p>
-        <p>vs.</p>
+        <p className="secondary">vs.</p>
         <p className="clickable" onClick={() => props.handleClick(props.player2)}>{props.player2})</p>
       </div>
       <div className="winner">
