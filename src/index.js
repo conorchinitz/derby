59205cc8b04959166fc34f4408a6f8fba1edf241
Conor Chinitz <48924938+conorchinitz@users.noreply.github.com>
2022-03-03 01:22:38 -0500

    Display game results horizontally.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App.jsx';
 
-  render(<App />, document.querySelector('#root'));
+render(<App />, document.querySelector('#root'));
